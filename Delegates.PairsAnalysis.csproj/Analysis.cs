838af6cbad28a60f19f8e722700c35b16353d6d6
aaltukhov <aaltukhov@acumatica.com>
2018-05-08 19:04:28 +0300

    implement lazy stuff

--- Delegates.PairsAnalysis.csproj/Analysis.cs
+++ Delegates.PairsAnalysis.csproj/Analysis.cs
@@ -8,6 +8,8 @@ namespace Delegates.PairsAnalysis
     {
         public static int FindMaxPeriodIndex(params DateTime[] data)
         {
+			//var x = data.ToPairs((date1, date2) => (date2 - date1).TotalSeconds);
+			//var y = data.ToPairs1((date1, date2) => (date2 - date1).TotalSeconds);
 
 			return data
 				.ToPairs((date1, date2) => (date2 - date1).TotalSeconds)
@@ -23,54 +25,147 @@ namespace Delegates.PairsAnalysis
 					(sum,count) => sum / count);
 		}
 
-		public static IEnumerable<Tout> ToPairs<Tin, Tout>(this IEnumerable<Tin> inputCollection, Func<Tin,Tin,Tout> process)
+		//public static IEnumerable<Tout> ToPairs1<Tin, Tout>(this IEnumerable<Tin> inputCollection, Func<Tin, Tin, Tout> process)
+		//{
+		//	List<Tin> inputCollectionToList = inputCollection.ToList();
+		//	if (inputCollectionToList.Count < 2)
+		//		throw new ArgumentException();
+		//	List<Tout> resultCollection = new List<Tout>();
+		//	for (int i = 0; i < inputCollectionToList.Count - 1; i++)
+		//		resultCollection.Add(process(inputCollectionToList[i], inputCollectionToList[i + 1]));
+		//	return resultCollection;
+		//}
+
+		//public static int MaxIndex<Tin>(this IEnumerable<Tin> inputCollection)
+		//	where Tin : IComparable
+		//{
+		//	List<Tin> inputCollectionToList = inputCollection.ToList();
+		//	if (inputCollectionToList.Count == 0) throw new ArgumentException();
+		//	var max = inputCollectionToList[0];
+		//	var bestIndex = 0;
+		//	for (int i = 0; i < inputCollectionToList.Count; i++)
+		//		if (inputCollectionToList[i].CompareTo(max) == 1)
+		//		{
+		//			max = inputCollectionToList[i];
+		//			bestIndex = i;
+		//		}
+		//	return bestIndex;
+		//}
+
+		//public static Tin AverageDifference<Tin>(this IEnumerable<Tin> inputCollection, Func<Tin, Tin, Tin> sumup, Func<Tin, int, Tin> average)
+		//{
+		//	List<Tin> inputCollectionToList = inputCollection.ToList();
+		//	Tin sum = default(Tin);
+		//	for (int i = 0; i < inputCollectionToList.Count; i++)
+		//		sum = sumup(sum, inputCollectionToList[i]);
+		//	return average(sum, inputCollectionToList.Count);
+		//}
+
+		//public static IEnumerable<Tuple<T, T>> Pairs<T>(this IEnumerable<T> inputCollection)
+		//{
+		//	List<T> inputCollectionToList = inputCollection.ToList();
+		//	if (inputCollectionToList.Count < 2)
+		//		throw new ArgumentException();
+		//	List<Tuple<T, T>> resultCollectionOfTuples = new List<Tuple<T, T>>();
+		//	for (int i = 0; i < inputCollectionToList.Count - 1; i++)
+		//	{
+		//		Tuple<T, T> currentTuple = Tuple.Create(inputCollectionToList[i], inputCollectionToList[i + 1]);
+		//		resultCollectionOfTuples.Add(currentTuple);
+		//	}
+		//	return resultCollectionOfTuples;
+		//}
+
+		public static IEnumerable<Tout> ToPairs<Tin, Tout>(this IEnumerable<Tin> inputCollection, Func<Tin, Tin, Tout> process)
 		{
-			List<Tin> inputCollectionToList = inputCollection.ToList();
-			if (inputCollectionToList.Count < 2)
-				throw new ArgumentException();
-			List<Tout> resultCollection = new List<Tout>();
-			for (int i = 0; i < inputCollectionToList.Count - 1; i++)
-				resultCollection.Add(process(inputCollectionToList[i], inputCollectionToList[i + 1]));
-			return resultCollection;
+			Tin previousItem = default(Tin);
+			bool isFirstItem = true;
+			bool emptyCollection = true;
+
+			foreach (var item in inputCollection)
+			{
+				if (isFirstItem)
+				{
+					previousItem = item;
+					isFirstItem = false;
+				}
+				else
+				{
+					yield return process(previousItem, item);
+					previousItem = item;
+					emptyCollection = false;
+				}
+			}
+
+			if (emptyCollection) throw new ArgumentException();
+
+
 		}
 
 		public static int MaxIndex<Tin>(this IEnumerable<Tin> inputCollection)
 			where Tin : IComparable
 		{
-			List<Tin> inputCollectionToList = inputCollection.ToList();
-			if (inputCollectionToList.Count == 0) throw new ArgumentException();
-			var max = inputCollectionToList[0];
-			var bestIndex = 0;
-			for (int i = 0; i < inputCollectionToList.Count; i++)
-				if (inputCollectionToList[i].CompareTo(max) == 1)
+			bool isFirstItem = true;
+			bool emptyCollection = true;
+			Tin maxItem = default(Tin);
+			int currentIndex = 0;
+			int maxIndex = 0;
+
+			foreach (var item in inputCollection)
+			{
+				if (isFirstItem)
 				{
-					max = inputCollectionToList[i];
-					bestIndex = i;
+					maxItem = item;
+					maxIndex = currentIndex;
+					isFirstItem = false;
+					emptyCollection = false;
 				}
-			return bestIndex;
+				else
+				{
+					if (item.CompareTo(maxItem) == 1)
+					{
+						maxItem = item;
+						maxIndex = currentIndex;
+					}
+				}
+				currentIndex++;
+			}
+			if (emptyCollection) throw new ArgumentException();
+			return maxIndex;
 		}
+
+
 
 		public static Tin AverageDifference<Tin>(this IEnumerable<Tin> inputCollection, Func<Tin,Tin,Tin> sumup, Func<Tin,int,Tin> average)
 		{
-			List<Tin> inputCollectionToList = inputCollection.ToList();
 			Tin sum = default(Tin);
-			for (int i = 0; i < inputCollectionToList.Count; i++)
-				sum = sumup(sum, inputCollectionToList[i]);
-			return average(sum, inputCollectionToList.Count);
+			int count = 0;
+			foreach (var item in inputCollection)
+			{
+				sum = sumup(sum, item);
+				count++;
+			}
+			return average(sum, count);
 		}
 
 		public static IEnumerable<Tuple<T, T>> Pairs<T>(this IEnumerable<T> inputCollection)
 		{
-			List<T> inputCollectionToList = inputCollection.ToList();
-			if (inputCollectionToList.Count < 2)
-				throw new ArgumentException();
-			List<Tuple<T, T>> resultCollectionOfTuples = new List<Tuple<T, T>>();
-			for (int i = 0; i < inputCollectionToList.Count-1; i++)
+			T previousItem = default(T);
+			bool isFirstItem = true;
+			bool emptyCollection = true;
+
+			foreach (var item in inputCollection)
 			{
-				Tuple<T, T> currentTuple = Tuple.Create(inputCollectionToList[i], inputCollectionToList[i + 1]);
-				resultCollectionOfTuples.Add(currentTuple);
+				if (isFirstItem)
+				{
+					previousItem = item;
+				}
+				else
+				{
+					yield return Tuple.Create(previousItem, item);
+					emptyCollection = false;
+				}
 			}
-			return resultCollectionOfTuples;
+			if (emptyCollection) throw new ArgumentException();
 		}
 	}
 
